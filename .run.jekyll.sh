9da4e4a38f04234e377d85ee541cde44ddc292ec
Julien Ponge <jponge@redhat.com>
2021-01-27 14:10:27 +0100

    Deploy with GH Actions (trial)

--- .run.jekyll.sh
+++ .run.jekyll.sh
@@ -1,5 +1,19 @@
 #/usr/bin/env bash
 set -e
 
+echo "⚙️  Building with Jekyll"
+gem install bundler
 bundle install
 bundle exec jekyll build
+
+echo "⚙️  Copying the files"
+rm -rf _gh_pages/*
+cp -R _site/* _gh_pages/
+
+echo "🚀 Commit and push"
+cd _gh_pages || exit
+git add -A
+git commit -m "Deploy the how-to pages"
+git push origin gh-pages
+
+echo "✅ Done"
